6de97efec7a8ba21032914a3cd410ffd1a506f18
Eronne <erwann.letue@gmail.com>
2017-11-14 16:56:01 +0100

    Binding data

--- app/src/main/java/com/example/eletue/application/Main2Activity.kt
+++ app/src/main/java/com/example/eletue/application/Main2Activity.kt
@@ -2,11 +2,17 @@ package com.example.eletue.application
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val percent = intent.getIntExtra("percent", 0)
+        findViewById<TextView>(R.id.percentValue).apply {
+            this.text = "Percent: $percent"
+        }
     }
 }
--- app/src/main/java/com/example/eletue/application/MainActivity.kt
+++ app/src/main/java/com/example/eletue/application/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val helloText : TextView = findViewById(R.id.hello)
 
+        val percent : TextView = findViewById(R.id.textView)
+
         val clickButton : Button = findViewById(R.id.button)
         clickButton.setOnClickListener {
             Toast.makeText(this, "You just clicked me!", Toast.LENGTH_SHORT).show()
-            val i = Intent(this.applicationContext, Main2Activity::class.java)
+            val i = Intent(this.applicationContext, Main2Activity::class.java).putExtra("percent", seekBar.progress)
             startActivity(i)
         }
-
-        val counter : TextView = findViewById(R.id.textView)
 
         val seekBar : SeekBar = findViewById(R.id.seekBar)
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar, p1: Int, p2: Boolean) {
-                counter.text = p1.toString() + " %"
+                percent.text = p1.toString()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
