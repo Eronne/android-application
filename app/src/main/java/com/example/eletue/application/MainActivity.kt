0038822476ffcc36707664177f3b239d6486ca08
Eronne <erwann.letue@gmail.com>
2017-11-14 15:07:46 +0100

    Home activity

--- app/src/main/java/com/example/eletue/application/MainActivity.kt
+++ app/src/main/java/com/example/eletue/application/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.eletue.application
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloText : TextView = findViewById(R.id.hello)
+        val clickButton : Button = findViewById(R.id.button)
+        clickButton.setOnClickListener {
+            Toast.makeText(this, "You just clicked me!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
